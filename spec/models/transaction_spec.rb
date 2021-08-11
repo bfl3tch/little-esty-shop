0beadec893c39741b685a427d98985b7bd6b452a
Brian Fletcher <brianpatrickfletcher@gmail.com>
2021-08-10 21:50:52 -0400

    Clean up transaction spec model

--- spec/models/transaction_spec.rb
+++ spec/models/transaction_spec.rb
@@ -7,10 +7,6 @@ RSpec.describe Transaction do
 
   describe 'validations' do
     it { should validate_presence_of :credit_card_number }
-    # it { should validate_presence_of :credit_card_expiration_date}
-    # validate on update? what about creating new transactions? existing records
-    # have no CC expiry date
-      # just a 'nice to have', circle back for 'back-fill' validation logic if time
     it { should validate_presence_of :result }
     it { should define_enum_for(:result).with_values([:success, :failed]) }
   end
